eb5d7c2021ce0c11d620ed165d157e62ebbac288
Guadalupe <guadalupe.alavila@gmail.com>
2024-02-21 15:39:39 -0300

    numeros

---
+++ section_2_tipos_basicos/06-numeros.py
@@ -0,0 +1,27 @@
+numero = 2  # entero integer
+
+decimal = 1.5  # decimal number
+
+imaginario = 2 + 3j  # 2 + 2i
+
+
+print(numero)
+print(decimal)
+print(imaginario)
+
+
+print(2+9)
+print(2-6)
+print(2*9)
+print(21/5)
+print(20//3)  # sin decimal
+print(20 % 2)  # modulo
+print(20 % 3)  # modulo, el resto
+print(2 ** 3)  # potencia
+print(9 ** 2)  # potencia
+
+numero += 4  # numero = numero + 4 es lo mismo
+print("numero es: ", numero)
+
+numero *= 4
+numero -= 4
