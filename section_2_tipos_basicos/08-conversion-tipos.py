86460aff2b7758257993338518923e122c3760a4
Guadalupe <guadalupe.alavila@gmail.com>
2024-02-21 17:07:18 -0300

    conversion tipos

---
+++ section_2_tipos_basicos/08-conversion-tipos.py
@@ -0,0 +1,16 @@
+
+
+# x = input("Ingrese un número: ")  # x es string
+
+# int() convierte a numero
+# str() convierte a string
+# float() convierte a float
+# bool() convierte a boolean
+
+
+print(bool(""))
+print(bool("0"))
+print(bool(None))
+print(bool(" "))
+print(bool(0))
+print(bool("true"))
