42d079cd760226a8905b9763c684ac6ffa817d4e
Guadalupe <guadalupe.alavila@gmail.com>
2024-02-21 15:31:11 -0300

    escape string

---
+++ section_2_tipos_basicos/05-secuencias-escape.py
@@ -0,0 +1,12 @@
+# esto es un comentario
+# \"
+# \'
+# \\
+# \n
+
+curso = 'Ultimate "Pyhton"'
+curso_slash = 'Ultimate \"Pyhton\"'
+curso_slash_ = '\n\nUltimate \nPyhton'
+print(curso)
+print(curso_slash)
+print(curso_slash_)
