462454a4eaaf281eaae674e99f2adbc79b3ec110
Guadalupe <guadalupe.alavila@gmail.com>
2024-02-21 15:49:36 -0300

    modulo math

---
+++ section_2_tipos_basicos/07-numeros-funciones.py
@@ -0,0 +1,27 @@
+# importar modulo Math
+
+import math
+
+
+print(round(3.141592))  # redondeo
+print(round(3.941592))
+
+
+print(abs(-190))  # valor absoluto
+print(abs(190))  # valor absoluto
+
+print("cuánto vale PI? ", math.pi)
+
+print("El nro mas cercano para arriba a 1.5 es:", math.ceil(1.5))
+
+print("El nro mas cercano para abajo a 1.5 es:", math.floor(1.5))
+
+
+print(math.isnan(4))
+# print(math.isnan("4"))
+
+print("2 elevado a 3 es: ", math.pow(2, 3))
+
+
+print("la raíz cuadrada de 81 es: ", math.sqrt(81))
+print("la raíz cuadrada de 144 es: ", math.sqrt(144))
